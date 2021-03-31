474027ba492c8c050775dcbb4664f00c975aace2
Guy Taylor <thebigguy.co.uk@gmail.com>
2021-03-31 17:30:38 +0100

    Add favicon
    Add source SVG, script to generate the icon and a icon file itself.

---
+++ icons/to-fav.sh
@@ -0,0 +1,7 @@
+convert favicon.svg  -background white \
+          \( -clone 0 -resize 16x16   -extent 16x16 \) \
+          \( -clone 0 -resize 32x32   -extent 32x32 \) \
+          \( -clone 0 -resize 48x48   -extent 48x48 \) \
+          \( -clone 0 -resize 64x64   -extent 64x64 \) \
+          \( -clone 0 -resize 128x128 -extent 128x128 \) \
+          -delete 0 -alpha off -colors 256 favicon.ico
